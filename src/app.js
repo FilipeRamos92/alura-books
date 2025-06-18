56f6e6c6517eca920a6b3f5618a11fafd099b188
Filipe <filipe.ramos92@hotmail.com>
2025-06-17 21:10:31 -0300

    Iniciando o uso do express no projeto e criando a rota de livros

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import express from "express";
+
+
+const app = express();
+app.use(express.json());
+
+const livros = [
+    {
+        id: 1,
+        titulo: "Biografia de Steve Jobs"
+    },
+    {
+        id: 2,
+        titulo: "Will Smith"
+    }
+];
+
+app.get("/", (req, res) => {
+    res.status(200).send("Curso de Node.js");
+});
+
+app.get("/livros", (req, res) => {
+    res.status(200).json(livros);
+});
+
+app.post("/livros", (req, res) => {
+    livros.push(req.body);
+    res.send("Livro cadastrado com sucesso");
+});
+
+export default app;

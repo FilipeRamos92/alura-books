4bb3b19badae2a4142ae0f2f3a5ba589d8347a4f
Filipe <filipe.ramos92@hotmail.com>
2025-06-23 20:30:58 -0300

    Finalizando o CRUD da rota livros

--- src/routers/livrosRouters.js
+++ src/routers/livrosRouters.js
@@ -4,6 +4,9 @@ import LivroController from "../controllers/livroController.js";
 const routes = express.Router();
 
 routes.get("/livros", LivroController.listarLivros);
+routes.get("/livros/:id", LivroController.buscarLivroPorId);
 routes.post("/livros", LivroController.cadastrarLivros);
+routes.put("/livros/:id", LivroController.atualizaLivro);
+routes.delete("/livros/:id", LivroController.deletaLivro);
 
 export default routes;
